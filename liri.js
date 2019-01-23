02206a2698801af1d6b889ca3ecfd788f5f1ee00
Kaleigh Herold <kaleigh224@gmail.com>
2019-01-22 19:55:33 -0500

    Got code working for movie this and concert this

--- liri.js
+++ liri.js
@@ -10,8 +10,9 @@ var spotify = new Spotify({
     secret: keys.spotify.secret
 });
 var bandsInTownURL = "https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp"
+
 var userInput = process.argv[2];
-var oMDBUrl = "http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy";
+//  var oMDBUrl = "http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy";
 
 var multiWord = process.argv.slice(3).join(" ");
 
@@ -24,12 +25,13 @@ if (userInput === "spotify-this-song" && process.argv[3]) {
     movieThis(process.argv[3])
 } else if (userInput === "movie-this" && !process.argv[3]) {
     movieThis("Mr. Nobody")
-} else if (userInput === "conert-this" && process.argv[3]) {
+} else if (userInput === "concert-this" && process.argv[3]) {
     concertThis(process.argv[3])
 } else if (userInput === "do-what-it-says") {
-    // read file random.txt
+    doWhatItSays()
 }
 
+// function for displaying Spotify info based on user input
 function spotifyThisSong(response) {
     spotify.search({ type: "track", query: response }, function (err, data) {
         if (err) {
@@ -41,34 +43,51 @@ function spotifyThisSong(response) {
 
 // axios request to OMDB API to grab and display:
 
-axios.get("http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy").then(
-    function movieThis(response) {
-        oMDBUrl.search({ })
-        if (response)
-        //    * Title of the movie.
-            console.log(response.Title)
-        //    * Year the movie came out.
-            console.log(response.Year)
-        //    * IMDB Rating of the movie.
-            console.log(response.Rating)
-        //    * Rotten Tomatoes Rating of the movie.
-        //    * Country where the movie was produced.
-            console.log(response.Country)
-        //    * Language of the movie.
-            console.log()
-        //    * Plot of the movie.
-        //    * Actors in the movie.
-    }
-)
+function movieThis() {
+    axios.get("http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy").then(function (response) {
 
-// axios request to bandsintown API to grab and display info of the band user inputs
-axios.get("https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp").then(
-    function concertThis(response) {
         if (response) {
-            console.log("Venue Name: " + results.value.name);
-            console.log("Location: " + results.venue.city);
-            console.log("Event Date: " + moment(results.datetime).format("MM/DD/YYYY"));
+            // //    * Title of the movie.
+            console.log("Title: " + response.data.Title)
+            // //    * Year the movie came out.
+            console.log("Year: " + response.data.Year)
+            // //    * IMDB Rating of the movie.
+            console.log("iMDB Rating: " + response.data.Ratings[0].Value)
+            // //    * Rotten Tomatoes Rating of the movie.
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
+            // //    * Country where the movie was produced.
+            console.log("Country: " + response.data.Country)
+            // //    * Language of the movie.
+            console.log("Language: " + response.data.Language)
+            // //    * Plot of the movie.
+            console.log("Plot: " + response.data.Plot)
+            // //    * Actors in the movie.
+            console.log("Actors: " + response.data.Actors)
 
         }
-    }
-)
+
+    });
+}
+
+// axios request to bandsintown API to grab and display info of the band user inputs
+function concertThis() {
+    var artist = process.argv[3];
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
+        for (var i = 0; i < response.data.length; i++) {
+
+            console.log("Venue Name: " + response.data[i].venue.name);
+            console.log("Location: " + response.data[i].venue.city);
+            console.log("Event Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+        }
+
+    })
+};
+
+// function to read the random.txt file
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function read(err, data) {
+        if (err) throw err;
+        console.log(data)
+    })
+}
+
